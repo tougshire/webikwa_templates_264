1e0dd6de5b145a349771c622da881f9c135effde
tougshire <tougshire@tougshire.com>
2026-05-07 04:26:37 -0400

    First commit

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WebikwaBaseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'webikwa_templates_264'
---
+++ models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, PublishingPanel
+from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
+from wagtail.fields import RichTextField, StreamField
+from wagtail.models import (
+    DraftStateMixin,
+    PreviewableMixin,
+    RevisionMixin,
+    TranslatableMixin,
+)
+from wagtail.snippets.models import register_snippet
+from wagtailmarkdown.fields import MarkdownField
+
+
+@register_setting
+class NavigationSettings(BaseGenericSetting):
+    twitter_url = models.URLField(verbose_name="Twitter URL", blank=True)
+    github_url = models.URLField(verbose_name="GitHub URL", blank=True)
+    mastodon_url = models.URLField(verbose_name="Mastodon URL", blank=True)
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("twitter_url"),
+                FieldPanel("github_url"),
+                FieldPanel("mastodon_url"),
+            ],
+            "Social settings",
+        )
+    ]
+
+
+@register_snippet
+class FooterText(
+    DraftStateMixin,
+    RevisionMixin,
+    PreviewableMixin,
+    TranslatableMixin,
+    models.Model,
+):
+
+    body = MarkdownField(
+        blank=True,
+        help_text="A markdown version of the body. Both this and the streamfield version body will be displayed if they have content",
+    )
+
+    panels = [
+        FieldPanel("body"),
+        PublishingPanel(),
+    ]
+
+    def __str__(self):
+        return "Footer text"
+
+    def get_preview_template(self, request, mode_name):
+        return "base.html"
+
+    def get_preview_context(self, request, mode_name):
+        return {"footer_text": self.body}
+
+    class Meta(TranslatableMixin.Meta):
+        verbose_name_plural = "Footer Text"
